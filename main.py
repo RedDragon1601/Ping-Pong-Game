35de1d3d33f41bb8f725aad911b926969512e2a0
RedDragon1601 <DragonRedChannel1601@gmail.com>
2020-12-05 15:55:40 +0700

    'Update'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle
 from player import*
 from functions import*
+import time
 
 sc = turtle.Screen()
 sc.title('Ping Pong')
@@ -10,15 +11,28 @@ sc.bgcolor('black')
 score_a = 0
 score_b = 0
 
+#create game border
+border_a = turtle.Turtle()
+border_a.speed(0)
+border_a.setposition(-390, -250)
+border_a.color('red')
+
+for i in range(1):
+    border_a.goto(-390, 250)
+    border_a.goto(390, 250)
+    border_a.goto(390, -250)
+    border_a.goto(-390, -250)
+    border_a.hideturtle()
+
 # Ball
 ball = turtle.Turtle()
 ball.speed(0)
-ball.shape("circle")
+ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 2
-ball.dy = -2
+ball.dx = 5
+ball.dy = -5
 
 # Pen
 pen = turtle.Turtle()
@@ -45,28 +59,29 @@ while True:
     ball.sety(ball.ycor() + ball.dy)
 
 	# Border checking
-    if ball.ycor() > 290:
-        ball.sety(290)
+    if ball.ycor() > 245:
+        ball.sety(245)
         ball.dy *= -1
 
-    if ball.ycor() < -290:
-        ball.sety(-290)
+    if ball.ycor() < -245:
+        ball.sety(-245)
         ball.dy *= -1
 
-    if ball.xcor() > 390:
-        ball.goto(0, 0)
+    if ball.xcor() > 385:
         ball.dx *= -1
         score_a += 1
         pen.clear()
         pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-
-    if ball.xcor() < -390:
+        time.sleep(1)
         ball.goto(0, 0)
+
+    if ball.xcor() < -385:
         ball.dx *= -1
         score_b += 1
         pen.clear()
         pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-
+        time.sleep(1)
+        ball.goto(0, 0)
 
     # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < player_2.ycor() + 40 and ball.ycor() > player_2.ycor() -40):
